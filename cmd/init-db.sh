3c5cdaaa0fadcd4328f240ea75a60c262133c02e
lancatlin <lancat1499@gmail.com>
2019-06-07 13:23:16 +0800

    完成初始化腳本，尚未接入 go init function

--- cmd/init-db.sh
+++ cmd/init-db.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
-if ["$EUID" -ne 0] then 
+if [ "$EUID" -ne 0 ]; then 
 	echo "Please run as root"
 	exit
 fi
+source .env
+echo $DB_NAME 
+echo $DB_USER
+echo $DB_PASSWORD
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
-DB_USER=$1
-DB_PASSWORD=$2
+mysql -D $DB_NAME < ./sql-command/init.sql
 
-mysql < ../sql-command/init.sql
-
-mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY $DB_PASSWORD"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost';"
